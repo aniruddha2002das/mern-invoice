d824af8768b3d222472d7d903f8d4bf44a40b0bd
Aniruddha <das.aniruddha4321@gmail.com>
2023-10-02 16:10:27 +0530

    feat: add common ui components and hooks

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -12,12 +14,14 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path='/*' element={<App/>}/>
+        </Routes>
+      </Router>
     </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
