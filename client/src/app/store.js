34ad3bb959f0eaff3ac5033303f6964e174cbc7d
Aniruddha <das.aniruddha4321@gmail.com>
2023-10-02 19:38:10 +0530

    feat: add auth and register api slices

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,7 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { baseApiSlice } from '../features/api/baseApiSlice';
+import authReducer from '../features/auth/authSlice';
+
 
 export const store = configureStore({
   reducer: {
-
+    [baseApiSlice.reducerPath]: baseApiSlice.reducer,
+    auth: authReducer
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApiSlice.middleware),
+  // TODO: change this to false in production
+  devTools: true,
 });
